f6d744c79e7a2aa4d93e4e7e5085bbfdca0703e4
Victoria Cope <copev@umich.edu>
2021-06-07 20:09:07 -0400

    hero gradient decrease size

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@
 $(document).mousemove(function(event) {
     windowWidth = $(window).width();
     windowHeight = $(window).height();
-    
+
     mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
     mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
-    
-    $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #f85032, #e4dbd7)');
-  });
+
+    $('.radial-gradient').css('background', 'radial-gradient(12rem at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #f85032, #f0edec)');
+});
